3cc3ef744ce3f7dbde5c6238122ea20e215ccf9e
“Matheus <matheus.ricci@hotmail.com>
2025-02-26 15:34:49 -0300

    Botao de cadastro funcional

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -21,20 +21,13 @@ public class conectaDAO {
         Connection conn = null;
         
         try {
-         
-              conn = null;
-              
-              Class.forName("com.mysql.cj.jdbc.Driver");
-              conn = DriverManager.getConnection("jdbc:mysql://localhost/etapa4", "root", "1234"); //Supersenha0112 ou 1234
-              System.out.println("Conectou com o banco de dados!!!!");
-          } catch (SQLException ex) {
-              System.out.println("Erro: Não foi possivel se conectar no banco de dados!");
-          } catch (ClassNotFoundException ex) {
-              System.out.println("Erro: Driver JDBC nao encontrado!.");
-          }       
-
-          return conn;
-      }
+        
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=1234");
+            
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
     }
     
 }
